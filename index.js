3a865ac43cca68c91f880373be942c544323bf2b
ducthuan1202 <ducthuan1202@gmail.com>
2018-10-11 23:22:00 +0700

    update code

--- index.js
+++ index.js
@@ -12,42 +12,38 @@ const
     app = express(),
     port = 3000,
     host = "127.0.0.1";
-
+/** app set */
 app.set("host", host);
 app.set("port", port);
-
-app.use("/", express.static(path.join(appRoot, "public")));
 app.set("views", path.join(appPath, "views"));
 app.set("view engine", "ejs");
+/** app use */
 app.use(flash());
-
+app.use("/", express.static(path.join(appRoot, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
     secret: "nauht-cud-neyugn-20210991",
     resave: false,
     saveUninitialized: false
 }));
-
+/** passport config */
 app.use(passport.initialize());
 app.use(passport.session());
-
 require('./app/passport')(passport);
-
-const routesFolder = path.join(appPath, "routes");
-
-app.use("/admin", isLoggedIn);
-
-if (fs.existsSync(routesFolder)) {
-    fs.readdirSync(routesFolder)
-        .filter(file => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
-        .map(file => {
-            require(path.join(routesFolder, file.slice(0, file.length - 3)))(app, isLoggedIn);
-        });
-}
-
-app.listen(port, () => console.log(`Server started at: <http://${host}:${port}>`));
-
-function isLoggedIn(req, res, next) {
+/** use middleware check login */
+const isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) next();
     else res.redirect('/login');
 }
+app.use("/admin", isLoggedIn);
+try {
+    const routesFolder = path.join(appPath, "routes");
+    if (fs.existsSync(routesFolder)) {
+        fs.readdirSync(routesFolder)
+            .filter(file => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js'))
+            .map(file => require(path.join(routesFolder, file.slice(0, file.length - 3)))(app, isLoggedIn));
+    }
+} catch (e) {
+    app.use("*", (req, res) => res.send({ action: 404, msg: e.message }));
+}
+app.listen(port, () => console.log(`Server started at: <http://${host}:${port}>`));
