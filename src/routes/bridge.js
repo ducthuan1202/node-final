98fff0aa571d3e3cfd03a097f092edf6c7ecf75c
Nguyen Duc Thuan <thuannd@qsoftvietnam.com>
2018-10-12 09:18:16 +0700

    update code

--- src/routes/bridge.js
+++ src/routes/bridge.js
@@ -7,24 +7,17 @@ const
     app = express();
 app.disable("etag");
 const
-    prefixApi = process.env.NODE_APP_API_PREFIX,
     prefixBackend = process.env.NODE_APP_BACKEND_PREFIX;
 const
-    folderApi = process.env.NODE_APP_API_FOLDER,
     folderBackend = process.env.NODE_APP_BACKEND_FOLDER,
     folderFrontend = process.env.NODE_APP_FRONTEND_FOLDER;
 const
     routesFolder = path.join(appRoot.toString(), "src", "routes"),
-    routesApi = path.join(routesFolder, folderApi),
     routeBackend = path.join(routesFolder, folderBackend),
     routeFrontend = path.join(routesFolder, folderFrontend);
 try {
     const isJsFile = (file) => (file.indexOf('.') !== 0) && (file.slice(-3) === '.js');
 
-    /** init routes api */
-    if (fs.existsSync(routesApi)) {
-        fs.readdirSync(routesApi).filter(file => isJsFile(file)).map(file => app.use(prefixApi, require(path.join(routesApi, file))));
-    }
     /** init routes backend */
     if (fs.existsSync(routeBackend)) {
         fs.readdirSync(routeBackend).filter(file => isJsFile(file)).map(file => app.use(prefixBackend, require(path.join(routeBackend, file))));
